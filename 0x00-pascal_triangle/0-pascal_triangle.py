16b6fb1c46998dc32d6f38c4d126c6618d5a8751
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-12-01 01:04:42 +0200

    create pascal function that return list of list of intger

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,3 +1,26 @@
-#!/usr/bin/env python3
-"""function represent pascal triangle """
+#!/usr/bin/python3
+""" module that form pascal function """
 
+
+def pascal_triangle(n):
+    """ function hat returns a list of lists of integers
+      representing the Pascal’s triangle of n
+      Params:
+      n: number of items in pascal triangle
+      Return:
+      list of list of integers or empty list
+    """
+    res = []
+    for i in range(n):
+        if i == 0:
+            res.append([1])
+        else:
+            li = res[i - 1]
+            newli = [1]
+            for index, num in enumerate(li):
+                if index + 1 < len(li):
+                    newli.append(num + li[index + 1])
+                else:
+                    newli.append(1)
+                    res.append(newli)
+    return res
