b9a50af06ebde3265e16d425e9db32f972adf648
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-12-17 21:24:45 +0200

    solve interview question

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
 """ interview problem module """
+
+
+def minOperations(n):
+    """calculte minmum number of operation"""
+    if n <= 1:
+        return 0
+    i = 2
+    res = 0
+    while i <= n:
+        if n % i == 0:
+            res += i
+            n = n / i
+        else:
+            i += 1
+    return res
