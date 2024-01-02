7cff239d860a0b7dc5c4eea429c3cbd833f7241c
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-01-02 11:03:05 +0200

    solve interview problem

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" module to solve interview question """
+
+
+def validUTF8(data):
+    """ function check if data is utf8 or not """
+    for i in range(len(data)):
+        if data[i] < 0 or data[i] > 255:
+            return False
+        if data[i] > 127 and data[i] < 256:
+            print(data[i])
+            num_bin = '{0:08b}'.format(data[i])
+            char_long = 8 if num_bin.find('0') == -1 else num_bin.find('0')
+            i = i + int(char_long) - 1
+        if i > len(data):
+            return False
+    return True
