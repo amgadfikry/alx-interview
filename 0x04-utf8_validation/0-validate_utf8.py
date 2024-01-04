77778b882d8b600ab2cd15f95a79302e387ad4c7
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-01-04 22:01:16 +0200

    change functionality of problem

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,15 +4,21 @@
 
 def validUTF8(data):
     """ function check if data is utf8 or not """
+    continous = 0
     for i in range(len(data)):
         if data[i] < 0 or data[i] > 255:
             return False
+        if continous > 0:
+            continous -= 1
+            num_bin = '{0:08b}'.format(data[i])
+            char_long = num_bin.find('0')
+            if char_long != 1:
+                return False
+            continue
         if data[i] > 127 and data[i] < 256:
             num_bin = '{0:08b}'.format(data[i])
             char_long = num_bin.find('0')
             if char_long > 4 or char_long < 2:
                 return False
-            i = i + int(char_long) - 1
-        if i >= len(data):
-            return False
+            continous += char_long
     return True
