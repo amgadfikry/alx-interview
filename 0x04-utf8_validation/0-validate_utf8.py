1cc872e3b287ebb8d75407dbc348fc3a725d9325
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-01-04 22:42:07 +0200

    imporove code readability and increase number of bytes

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,25 +4,27 @@
 
 def validUTF8(data):
     """ function check if data is utf8 or not """
-    continuation_bytes = 0
+    continous_bytes = 0
+
     for byte in data:
         if byte < 0 or byte > 255:
             return False
-        leading_ones = bin(byte)[2:].count('1')
 
-        if leading_ones == 0:
-            continuation_bytes = 0
-            continue
+        byte_bin = '{0:08b}'.format(byte)
+        leading_ones = byte_bin.find('0')
 
-        if continuation_bytes > 0:
+        if continous_bytes > 0:
             if leading_ones != 1:
                 return False
-            continuation_bytes -= 1
+            continous_bytes -= 1
             continue
 
-        if leading_ones > 4 or leading_ones == 1:
+        if leading_ones == 0:
+            continue
+
+        if leading_ones > 4:
             return False
 
-        continuation_bytes = leading_ones - 1
+        continous_bytes = leading_ones - 1
 
-    return continuation_bytes == 0
+    return continous_bytes == 0
