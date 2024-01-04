84c4a95dd5ab6737c103c3a466d89a4a521c1d19
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-01-04 22:13:14 +0200

    fix number of char long abd remove unnessecary loops

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,20 +5,19 @@
 def validUTF8(data):
     """ function check if data is utf8 or not """
     continous = 0
-    for i in range(len(data)):
-        if data[i] < 0 or data[i] > 255:
+    for num in data:
+        if num < 0 or num > 255:
             return False
+        num_bin = '{0:08b}'.format(num)
+        char_long = num_bin.find('0')
         if continous > 0:
             continous -= 1
-            num_bin = '{0:08b}'.format(data[i])
-            char_long = num_bin.find('0')
             if char_long != 1:
                 return False
             continue
-        if data[i] > 127:
-            num_bin = '{0:08b}'.format(data[i])
-            char_long = num_bin.find('0')
-            if char_long not in [2, 3, 4]:
-                return False
-            continous = char_long - 1
+        if char_long > 3:
+            return False
+        if char_long == 0:
+            continue
+        continous = char_long - 1
     return True
