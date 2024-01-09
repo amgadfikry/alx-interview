1608175ce09fc0361e0a509f98e432de37e4cc0a
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-01-10 01:13:00 +0200

    second try to solve problem

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -21,17 +21,22 @@ def nqueens():
         print('N must be at least 4')
         exit(1)
 
+    first_col = 1 if chess_num % 2 == 0 else 0
+    end_col = chess_num - 1 if chess_num % 2 == 0 else chess_num
     incr = 2
-    for i in range(1, chess_num - 1):
+    for i in range(first_col, end_col):
         result = []
         col = i
         result.append([0, i])
         for row in range(1, chess_num):
             col += incr
-            if col >= chess_num:
+            if col >= chess_num and first_col == 1:
                 col -= (chess_num + 1)
+            if col >= chess_num and first_col == 0:
+                col -= chess_num
             result.append([row, col])
-        incr += 1
+        if first_col == 1:
+            incr += 1
         print(result)
 
 
