579dfef6e96f9a03ba2156b718a8dace0355e3fa
amgadfikry <dr.amgad_sh92@yahoo.com>
2024-01-10 01:16:18 +0200

    second try to solve problem

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -18,7 +18,7 @@ def nqueens():
         exit(1)
 
     if chess_num < 4:
-        print(' must be at least 4')
+        print('N must be at least 4')
         exit(1)
 
     first_col = 1 if chess_num % 2 == 0 else 0
